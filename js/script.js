74bad248f6f18540dc6f16595b56d9e54e388083
Tomasz Szkudlarek <Tomasz.szkudlarek25@gmail.com>
2022-11-26 18:14:27 +0100

    let to const change

--- js/script.js
+++ js/script.js
@@ -1,31 +1,34 @@
-let button = document.querySelector(".nav__button");
-let body = document.querySelector(".js-bodyBackground")
-let darkButton = document.querySelector(".js-nav__color")
-let ex = document.querySelector(".day__select");
+{
+    {
+        const button = document.querySelector(".nav__button");
+        const body = document.querySelector(".js-bodyBackground");
+        const darkButton = document.querySelector(".js-nav__color");
+        const ex = document.querySelector(".day__select");
 
-button.addEventListener("click", () => {
-    body.classList.toggle("dark");
-    button.classList.toggle("nav__button--bright")
-    if (body.classList.contains("dark")) {
-        darkButton.innerText = "jasny"
-    } else {
-        darkButton.innerText = "ciemny"
+        button.addEventListener("click", () => {
+
+            body.classList.toggle("dark");
+            button.classList.toggle("nav__button--bright")
+            if (body.classList.contains("dark")) {
+                darkButton.innerText = "jasny"
+            } else {
+                darkButton.innerText = "ciemny"
+            }
+        });
     }
-});
 
-const excercisesButtonElement = document.querySelector('#js-exercises__button');
-const excercisesChooseElement = document.querySelector('#js-exercises__choose')
-let excercisePickedElement = document.querySelector('.js-exercises__ex')
+    {
+        const excercisesButtonElement = document.querySelector('#js-exercises__button');
+        const excercisesChooseElement = document.querySelector('#js-exercises__choose');
+        const excercisePickedElement = document.querySelector('.js-exercises__ex');
 
-excercisesButtonElement.onclick = (event) => {
-    event.preventDefault();
-    excercisePickedElement.innerHTML = excercisesChooseElement.value;
-};
+        excercisesButtonElement.onclick = (event) => {
+            event.preventDefault();
+            excercisePickedElement.innerHTML = excercisesChooseElement.value;
+        };
 
-let response = document.querySelector(".container__response").innerText
-let responseTable = document.querySelector(".container__table")
+        const response = document.querySelector(".container__response").innerText;
+        const responseTable = document.querySelector(".container__table");
+    }
 
-
-
-
-
+}
